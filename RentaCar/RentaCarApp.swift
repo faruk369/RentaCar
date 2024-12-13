515a127e85be24ff6bbcd70d387ffca652638db5
Umar Faruk Nasir <86917307+faruk369@users.noreply.github.com>
2024-12-13 18:25:09 +0300

    Initial Commit

---
+++ RentaCar/RentaCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentaCarApp.swift
+//  RentaCar
+//
+//  Created by Faryk on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct RentaCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
